96a8a1fce676dba5e399f19f71f5ddf7140ba172
luisgbr1el <harrycorpmail@gmail.com>
2022-08-18 21:26:18 -0300

    Adicionando efeitos de texto e títulos

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,20 @@
 <template>
-   <Header />
-        <br/>
-        <h1 className='flex'>E então, você está pronto<br/>para escolher seu lado?</h1>
-        <a class="button" href="https://discord.gg/webbcenter" target="_blank" style="--clr:#1e9bff"><span>ANDRÔMEDA</span><i></i></a>
-        <a class="button" href="https://discord.gg/webbcenter" target="_blank" style="--clr:#ff1867"><span>BELLATRIX</span><i></i></a>
+  <Header />
+  <br/>
+  <h1 class="title">
+    <span class="text">Bem-vindo ao</span>
+    <br>
+    <span class="title">Webb Center</span>
+  </h1>
+  <h1 className='flex'>E então, você está pronto<br/>para escolher seu lado?</h1>
+  <a class="button" href="https://discord.gg/webbcenter" target="_blank" style="--clr:#1e9bff"><span>ANDRÔMEDA</span><i></i></a>
+  <a class="button" href="https://discord.gg/webbcenter" target="_blank" style="--clr:#ff002a"><span>BELLATRIX</span><i></i></a>
   
+  <section id="characters">
+    <div class="characters">
+      <!-- oi -->
+    </div>
+  </section>
 </template>
 
 <script>
--- src/main.js
+++ src/main.js
@@ -4,5 +4,6 @@ import App from './App.vue'
 require('./assets/styles/header.css');
 require('./assets/styles/global.css');
 require('./assets/styles/teamButton.css');
+require('./assets/styles/title.css');
 
 createApp(App).mount('#app')
