8b9b1ba775ac04042bec452da425a5616eb5f8a9
Júnior Veras <iramarjunhorveras@gmail.com>
2022-08-19 00:06:28 -0300

    Stars

--- src/main.js
+++ src/main.js
@@ -5,5 +5,7 @@ require('./assets/styles/header.css');
 require('./assets/styles/global.css');
 require('./assets/styles/teamButton.css');
 require('./assets/styles/title.css');
+require('./assets/styles/stars.css');
+require('./assets/scripts/stars.js');
 
 createApp(App).mount('#app')
